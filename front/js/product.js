2782c70f848358a933a3f875654a1362fbc3ebc1
Sabrina Gadaleta <sabrinagadaleta@gmail.com>
2022-03-17 12:50:52 +0100

    corrections

--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,22 @@
-let str = window.location.href;
-let url = new URL(str);
+//Récupération de l'id dans l'url
+let queryString = window.location.href;
+let url = new URL(queryString);
 let productId = url.searchParams.get("id");
+console.log(productId);
 
+// Récupération des articles de l'API grace a l'ID. 
 fetch("http://localhost:3000/api/products/" + productId)
-.then(function (response) {
-    return response.json();
+.then(function (res) {
+    return res.json();
 })
-.catch((error) => {
-    console.log("Erreur");
+.then(function(value) {
+    viewProduct(value);
+})
+.catch(function(err) {
 })
 
 // Inserer les données de l'API dans le HTML (DOM) grace à la fonction viewProduct.
-.then(function (viewProduct) {
-    const product = viewProduct;
-    console.log(product);
+function viewProduct (product) {
 
     // Insertion de l'image
     let productImg = document.createElement("img");
@@ -33,14 +36,13 @@ fetch("http://localhost:3000/api/products/" + productId)
     let productDescription = document.querySelector('#description');
     productDescription.innerHTML = product.description;
 
-    // Options de couleur. la boucle for parcours tous les éléments du key = 'colors'
+    // Options de couleur
     // La variable productColors insere les options de couleurs dans le HTML (DOM)
 
         for (let colors of product.colors){
-        console.log(colors);
         let productColors = document.createElement("option");
             document.querySelector("#colors").appendChild(productColors);
             productColors.value = colors;
     productColors.innerHTML = colors;
     }
-});
+};
